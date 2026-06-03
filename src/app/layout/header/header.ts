71e02a79064a8009d00962ed25ebb02536e68669
Leonhard Siegele <leonhard.siegele39@gmail.com>
2026-06-04 00:58:22 +0200

    first commit

---
+++ src/app/layout/header/header.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  btnText = "Neues Produkt";
+  btnClassList = "btn btn-primary";
+  path = "";
+
+  ngOnInit() {
+    this.path = "detail";
+    if(this.path === ""){
+      this.btnText = "Zurück zur Übersicht";
+    }
+  }
+}
